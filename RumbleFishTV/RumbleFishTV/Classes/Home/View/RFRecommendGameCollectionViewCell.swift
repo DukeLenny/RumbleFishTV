0e3102d76d60440fcb1bfc7ff9295f3bec02da29
DukeLenny <1637549689@qq.com>
2018-10-25 18:12:44 +0800

    首页推荐游戏视图添加collectionView.

---
+++ RumbleFishTV/RumbleFishTV/Classes/Home/View/RFRecommendGameCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  RFRecommendGameCollectionViewCell.swift
+//  RumbleFishTV
+//
+//  Created by LiDinggui on 2018/10/25.
+//  Copyright © 2018年 MKTECH. All rights reserved.
+//
+
+import UIKit
+
+class RFRecommendGameCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- RumbleFishTV/RumbleFishTV/Classes/Home/View/RFRecommendGameView.swift
+++ RumbleFishTV/RumbleFishTV/Classes/Home/View/RFRecommendGameView.swift
@@ -8,7 +8,11 @@
 
 import UIKit
 
+private let kCellId = "RFRecommendGameCollectionViewCell"
+
 class RFRecommendGameView: UIView {
+    
+    @IBOutlet private weak var collectionView: UICollectionView!
     
     class func instance() -> RFRecommendGameView {
         return Bundle.main.loadNibNamed("RFRecommendGameView", owner: nil, options: nil)?.first as! RFRecommendGameView
@@ -18,8 +22,23 @@ class RFRecommendGameView: UIView {
         super.awakeFromNib()
         
         autoresizingMask = UIViewAutoresizing()
+        
+        collectionView.register(UINib(nibName: "RFRecommendGameCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: kCellId)
     }
     
     
 
+}
+
+extension RFRecommendGameView: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 12
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: kCellId, for: indexPath) as! RFRecommendGameCollectionViewCell
+        cell.contentView.backgroundColor = UIColor.random()
+        
+        return cell
+    }
 }
